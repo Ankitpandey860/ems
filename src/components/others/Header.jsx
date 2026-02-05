b6d2f477c599ed0239bd266700455e712c9c42bb
ankitpadey860 <ankitp6040@gmail.com>
2026-02-05 17:10:08 +0530

    project ems

---
+++ src/components/others/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='flex items-end justify-between'>
+
+      <h1>hello dost </h1>
+      <button>log out</button>
+    </div>
+  )
+}
+
+export default Header
